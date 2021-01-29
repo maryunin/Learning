0f93b26d9b3eab92fd131a59d64bfc0da2a85afa
maryunin <maryunin@gmail.com>
2021-01-29 18:58:20 +0200

    changes by AKOK

---
+++ task21/task21/CodeBase/Controllers/MemberController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+using CodeBase.Models;
+using System.Web.Routing;
+
+namespace task21.Controllers
+{
+    public class MemberController : SurfaceController
+    {
+
+        public const string Name = "member";
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult SubmitLogin(LoginModel model, string returnUrl = null)
+        {
+            if (!Membership.ValidateUser(model.Username, model.Password))
+                ModelState.AddModelError("", Umbraco.GetDictionaryValue("Frontend.Account.Login.MemberPasswordError"));
+            if (!ModelState.IsValid) return CurrentUmbracoPage();
+
+            //Microsoft.Identity
+            FormsAuthentication.SetAuthCookie(model.Username, false);
+            UrlHelper myHelper = new UrlHelper(HttpContext.Request.RequestContext);
+            if (!string.IsNullOrWhiteSpace(returnUrl))
+                return Redirect(returnUrl);
+            return CurrentUmbracoPage();
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult SubmitLogout()
+        {
+            TempData.Clear();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+
+
+}
